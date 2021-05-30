ffb9dcbb1ecc7dead4dc359ed1724dd28b946f90
Douglas <douglas.hfonseca@gmail.com>
2021-05-30 18:20:05 -0300

    criando estruturas

---
+++ src/structs/biblioteca.c
@@ -0,0 +1,25 @@
+typedef struct data
+{
+  int dia;
+  int mes;
+  int ano;
+} TDate;
+
+typedef struct livro
+{
+  char ISBN[20];
+  char titulo[100];
+  char editora[100];
+  char local[100];
+  TDate data_pub;
+  char prim_autor[100];
+  char seg_autor[100];
+  int quantidade;
+  int quant_emprestados;
+} TLivro;
+
+typedef struct biblioteca
+{
+  TLivro livros[100];
+  int indice;
+} TBiblioteca;
---
+++ src/structs/usuario.c
@@ -0,0 +1,26 @@
+typedef struct data
+{
+  int dia;
+  int mes;
+  int ano;
+} TDate;
+
+typedef struct endereco
+{
+  char loagadrouro[100];
+  int numero;
+  char bairro[100];
+  char cidade[100];
+  char cep[10];
+} TEndereco;
+
+typedef struct usuario
+{
+  char cpf;
+  char nome;
+  char sobrenome;
+  char identidade;
+  TEndereco endereco;
+  TDate data_nascimento;
+  char tipo_usuario;
+};
